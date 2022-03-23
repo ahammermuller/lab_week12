327e3612fbc3a4fc2ac36edc37e9acc4431701f9
ahammermuller <98852797+ahammermuller@users.noreply.github.com>
2022-03-23 13:07:36 -0700

    created a function applydarkmode to apply a dark mode to the index page

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 const divlightTheme = document.querySelector('.container light-theme')
 console.log(divlightTheme)
 
+
+
+
+
+function applydarkmode(){
+    const dark_mode = document.querySelector('.light-theme')
+    dark_mode.classList.add('dark-theme')
+}
+applydarkmode()
